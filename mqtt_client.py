98818eefb1d24d078ebf4eb3dc358cf8ed99511e
Kaiyuan Wang <me@iamkaiyuan.wang>
2019-09-03 21:53:20 +0800

    updated mqtt class

--- mqtt_client.py
+++ mqtt_client.py
@@ -12,11 +12,34 @@ class MQTT:
         username = settings.get('username')
         password = settings.get('password')
         client_id = 'hydrometer-' + str(ubinascii.hexlify(machine.unique_id()), 'UTF-8')
+        self.enabled = settings.get('enabled')
+        self.interval_ms = settings.get('pubIntervalMs')
         self.topic = settings.get('topic')
         if self.topic.endswith('/'):
             self.topic = self.topic[:-1]
         self.client = MQTTClient(client_id, server, port, username, password)
         self.client.connect()
 
-    def publish(self, str_data):
-        self.client.publish(self.topic, str_data)
+    def is_enabled(self):
+        return self.enabled
+
+    def get_update_interval_ms(self):
+        return self.interval_ms
+
+    def connect(self):
+        try:
+            self.client.connect()
+        except:
+            print('Failed to connect to the MQTT broker.')
+
+    def disconnect(self):
+        self.client.disconnect()
+
+    def publish(self, str_msg):
+        try:
+            self.connect()
+        except:
+            pass
+        else:
+            self.client.publish(self.topic, str_msg)
+            self.disconnect()
