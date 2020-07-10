4ba797f5e22a17d2fe85d2c29c01217aacbda857
Kaiyuan <me@iamkaiyuan.wang>
2020-07-10 11:50:33 +0800

    mqtt_client changed: was umqtt.simple, now is umqtt.simple2 (https://github.com/fizista/micropython-umqtt.simple2) which solved the issue that when connecting to certain mqtt server using umqtt.simple error will occur.

---
+++ torpedo/lib/umqtt/errno.py
@@ -0,0 +1,27 @@
+EUNKNOWN = -1   # Unknown
+ECONCLOSE = 1   # Connection closed by host
+EREADLEN = 2    # Wrong length of read data
+EWRITELEN = 3   # Wrong length of read data
+ESTRTOLONG = 4  # String too long !!!!
+ERESPONSE = 6   # Wrong response
+EKEEPALIVE = 7  # Connection keep time has been exceeded (umqtt.robust2)
+ENOCON = 8      # No connection
+
+ECONUNKNOWN = 20     # Connection refused, unknown error
+ECONPROTOCOL = 21    # Connection refused, unacteptable protocol version
+ECONREJECT = 22      # Connection refused, identifier rejected
+ECONUNAVAIBLE = 23   # Connection refused, server unavaible
+ECONCREDENTIALS = 24 # Connection refused, bad credentials
+ECONAUTH = 25        # Connection refused, not authorized
+ECONNOT = 28         # No connection
+ECONLENGTH = 29      # Connection, control packet type, Remaining Length != 2
+ECONTIMEOUT = 30     # Connection timeout
+
+ESUBACKUNKNOWN = 40  # Subscribe confirm unknown fail, SUBACK
+ESUBACKFAIL = 44     # Subscribe confirm response: Failure
+
+# Status code numbers from set_callback_status()
+STIMEOUT = 0    # timeout
+SDELIVERED = 1  # successfully delivered
+SUNKNOWNPID = 2 # Unknown PID. It is also possible that the PID is outdated,
+                # i.e. it came out of the message timeout.
--- torpedo/mqtt_client.py
+++ torpedo/mqtt_client.py
@@ -1,7 +1,7 @@
-import machine
-import ubinascii
+import utime
 
-from umqtt.robust import MQTTClient
+# from umqtt.robust import MQTTClient
+from lib.umqtt.simple2 import MQTTClient
 
 
 class MQTT:
@@ -11,13 +11,22 @@ class MQTT:
         port = settings.get('brokerPort')
         username = settings.get('username')
         password = settings.get('password')
-        client_id = 'hydrometer-' + str(ubinascii.hexlify(machine.unique_id()), 'UTF-8')
+        # client_id = 'hydrometer-' + str(ubinascii.hexlify(machine.unique_id()), 'UTF-8')
+        client_id = settings.get('clientId')
         self.enabled = settings.get('enabled')
         self.interval_ms = settings.get('pubIntervalMs')
-        self.topic = settings.get('topic')
-        if self.topic.endswith('/'):
-            self.topic = self.topic[:-1]
-        self.client = MQTTClient(client_id, server, port, username, password)
+        the_topic = settings.get('topic')
+        if the_topic.endswith('/'):
+            the_topic = the_topic[:-1]
+        self.topic = str.encode(the_topic) if isinstance(the_topic, str) else the_topic
+        self.client = MQTTClient(
+            client_id=client_id,
+            server=server,
+            port=port,
+            user=username,
+            password=password,
+            keepalive=10
+        )
 
     def is_enabled(self):
         return self.enabled
@@ -43,8 +52,13 @@ class MQTT:
     def publish(self, str_msg):
         try:
             self.connect()
+            utime.sleep_ms(1000)
         except:
             print('Failed to publish the data to the MQTT broker.')
         else:
-            self.client.publish(self.topic, str_msg)
+            msg = str.encode(str_msg) if isinstance(str_msg, str) else str_msg
+            print(msg)
+            self.client.publish(self.topic, msg)
+            utime.sleep_ms(1000)
             self.disconnect()
+            print('Data have been sent to MQTT broker.')
